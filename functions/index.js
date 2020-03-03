e3204a9cdc042ee39f02f562ccc7e72cdc34c020
Will Burr <williamburr98@gmail.com>
2020-03-03 01:25:56 +0000

    Revert "Fix typo"
    This reverts commit b61fd18c55a1439eecbbdb966c2aee70963ea86e.
    
    # Conflicts:
    #	functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -1,62 +1,72 @@
+const admin = require("firebase-admin");
 const functions = require('firebase-functions');
 const {dialogflow} = require('actions-on-google');
 const app = dialogflow({ debug: true });
 
-app.intent('Welcome Intent', async (conv) => {
-  if (conv.user.storage.activities) {
-    conv.ask("Hi again! How can I help?");
-  } else {
-    conv.ask("Hi. I am Profiler, why don't you ask me to start profiling a new activity?");
-    conv.user.storage.activities = {};
-  }
-});
+admin.initializeApp(functions.config().firebase);
+
+let db = admin.firestore();
+
 
 app.intent('Add Activity', async (conv, {activity_name}) => {
-  conv.user.storage.activities[activity_name] = {
+  let collectionRef = db.collection('activites');
+  await collectionRef.add({
+    name: activity_name,
     total_seconds: 0,
     start_time: null
-  };
+  });
   conv.ask(`Successfully added a new activity called: ${activity_name}`);
 });
 
 app.intent('List Activities', async (conv) => {
-  const names = conv.user.storage.activities.keys().map(activity => activity.name);
+  const activities = await db.collection('activities').get();
+  const names = activities.docs.map(doc => doc.data().name);
   conv.ask(`Your profiled activities are: ${names}`);
 });
 
 app.intent('Start Activity', async (conv, {activity_name}) => {
-  if (!(activity_name in conv.user.storage.activities)) {
-    conv.ask(`No activity named ${activity_name}`);
+  const activities = await db.collection('activities').where('name', '==', activity_name).limit(1).get();
+  if (activities.empty) {
+    conv.ask(`No activity named: ${activity_name}`);
+    return;
   }
-  conv.user.storage.activities[activity_name].start_time = Date.now();
+  const timeSink = activities.docs[0];
+  const timeSinkRef = timeSink.ref;
+  await timeSinkRef.set({
+    start_time: Date.now()
+  }, {merge: true});
   conv.ask(`Started profiling activity: ${activity_name}`);
 });
 
 app.intent('Stop Activity', async (conv, {activity_name}) => {
-  const activities = conv.user.storage.activities;
-  if (!(activity_name in activities)) {
-    conv.ask(`No activity named ${activity_name}`);
+  const activities = await db.collection('activities').where('name', '==', activity_name).limit(1).get();
+  if (activities.empty) {
+    conv.ask(`No activity named: ${activity_name}`);
+    return;
   }
-  const activity = activities[activity_name];
-  const startTime = activity.start_time;
-  const totalSeconds = activity.total_seconds;
+  const activity = activities.docs[0];
+  const startTime = activity.data().start_time;
+  const totalSeconds = activity.data().total_seconds;
   if (startTime === null) {
     conv.ask(`You have not started ${activity_name} yet.`);
     return;
   }
   const sessionSeconds = Math.floor((Date.now() - startTime) /1000);
   const seconds = totalSeconds + sessionSeconds;
-  conv.user.storage.activities[activity_name].start_time = null;
-  conv.user.storage.activities[activity_name].total_seconds = seconds;
+  await activity.ref.set({
+    start_time: null,
+    total_seconds: seconds
+  }, {merge: true});
   conv.ask(`You spent ${secondsToTimePhrase(sessionSeconds)} ${activity_name}`);
 });
 
 app.intent('How Long Have I Spent', async (conv, {activity_name}) => {
-  const activities = conv.user.storage.activities;
-  if (!(activity_name in activities)) {
-    conv.ask(`No activity named ${activity_name}`);
+  const activities = await db.collection('activities').where('name', '==', activity_name).limit(1).get();
+  if (activities.empty) {
+    conv.ask(`No activity named: ${activity_name}`);
+    return;
   }
-  const seconds = activities[activity_name].total_seconds;
+  const seconds = activities.docs[0].data().total_seconds;
   conv.ask(`You have spent a total of ${secondsToTimePhrase(seconds)} ${activity_name}`);
 });
 
